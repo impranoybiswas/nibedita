afd54a6a41a5537cb3b20ccea19eea708af63302
Pranoy Biswas Bappa <impranoybiswas@gmail.com>
2026-06-04 00:10:50 +0600

    update button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,6 +20,30 @@ const streamUrls = [
     name: "Test Channel 4",
     url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1703/output/index.m3u8",
   },
+  {
+    name: "Test Channel 5",
+    url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1704/output/index.m3u8",
+  },
+  {
+    name: "Test Channel 6",
+    url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1706/output/index.m3u8",
+  },
+  {
+    name: "Test Channel 7",
+    url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1722/output/index.m3u8",
+  },
+  {
+    name: "Test Channel 8",
+    url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1716/output/index.m3u8",
+  },
+  {
+    name: "Test Channel 9",
+    url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1715/output/index.m3u8",
+  },
+  {
+    name: "Test Channel 10",
+    url: "https://owrcovcrpy.gpcdn.net/bpk-tv/1723/output/index.m3u8",
+  },
 ];
 
 export default function HomePage() {
@@ -33,9 +57,9 @@ export default function HomePage() {
           channelName="Test Channel"
         />
       </div>
-      <div>
+      <div className="flex flex-wrap gap-3 items-center">
         {streamUrls.map((url, index) => (
-          <button onClick={() => setCurrentStream(url)}>{url.name}</button>
+          <button className={`bg-blue-400 text-white px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-600 transition-colors ${currentStream.url === url.url ? "bg-blue-600" : ""}`} key={url.url} onClick={() => setCurrentStream(url)}>{url.name}</button>
         ))}
       </div>
     </div>
